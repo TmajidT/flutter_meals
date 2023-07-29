1756453cf91581a7ee8f9b119c57707b53c069f7
Majid <majid.asfanani@gmail.com>
2023-07-29 12:53:07 +0330

    adding meals

---
+++ lib/screens/meals.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:meals/models/meal.dart';
+
+class MealsScreen extends StatelessWidget {
+  const MealsScreen({
+    super.key,
+    required this.meals,
+    required this.title,
+  });
+
+  final String title;
+  final List<Meal> meals;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(title),
+      ),
+      body: ,
+    );
+  }
+}
