250dbd2c8c153e6f677ff8488da403cbd22f13d4
Majid <majid.asfanani@gmail.com>
2023-07-30 12:39:04 +0330

    added cards-made items with pictures

--- lib/screens/meals.dart
+++ lib/screens/meals.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:meals/main.dart';
 import 'package:meals/models/meal.dart';
+import 'package:meals/widgets/meal_item.dart';
 
 class MealsScreen extends StatelessWidget {
   const MealsScreen({
@@ -40,9 +41,7 @@ class MealsScreen extends StatelessWidget {
     if (meals.isNotEmpty) {
       content = ListView.builder(
         itemCount: meals.length,
-        itemBuilder: (ctx, index) => Text(
-          meals[index].title,
-        ),
+        itemBuilder: (ctx, index) => MealItem(meal: meals[index]),
       );
     }
 
