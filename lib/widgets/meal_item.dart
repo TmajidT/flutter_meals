6e1831a49fb0cdc4b10dbcb184be371172766d12
Majid <majid.asfanani@gmail.com>
2023-07-31 12:01:16 +0330

    added details to the cards and food items

--- lib/widgets/meal_item.dart
+++ lib/widgets/meal_item.dart
@@ -1,11 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:meals/models/meal.dart';
+import 'package:meals/widgets/meal_item_trait.dart';
 import 'package:transparent_image/transparent_image.dart';
 
 class MealItem extends StatelessWidget {
   const MealItem({super.key, required this.meal});
 
   final Meal meal;
+
+  String get complexityText {
+    return meal.complexity.name[0].toUpperCase() +
+        meal.complexity.name.substring(1);
+  }
+    String get affordabilityText {
+    return meal.affordability.name[0].toUpperCase() +
+        meal.affordability.name.substring(1);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -51,7 +61,27 @@ class MealItem extends StatelessWidget {
                     height: 12,
                   ),
                   Row(
-                    children: [],
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      MealItemTrait(
+                        icon: Icons.schedule,
+                        lable: '${meal.duration} min',
+                      ),
+                      const SizedBox(
+                        width: 10,
+                      ),
+                      MealItemTrait(
+                        icon: Icons.work,
+                        lable: complexityText,
+                      ),
+                      const SizedBox(
+                        width: 10,
+                      ),
+                      MealItemTrait(
+                        icon: Icons.attach_money,
+                        lable: affordabilityText,
+                      ),
+                    ],
                   )
                 ]),
               ),
